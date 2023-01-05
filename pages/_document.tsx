53bac35dddc46480a0e14bf2f2b2afe5eb9dacb8
Angie Zhou <angiezhou2@gmail.com>
2023-01-05 15:27:36 -0800

    second commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Roboto:ital,wght@0,400;0,500;1,400&display=swap" rel="stylesheet"/>
+                <link href="https://fonts.googleapis.com/css2?family=Jost&display=swap" rel="stylesheet"/>
             </Head>
             <body>
                 <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,13 +9,40 @@ export default function Index() {
         </Head>
         <div id="container">
             <div id="top">
-                <h1>Home</h1>
-                <hr/>
-                <p>This is the home page!!!</p>
+                <img src="/angiez.jpg" alt="Image of Angie Zhou" id="angie-z" />
+            </div>
+            <div id="middle">
+                <h1>Hi, I'm Angie!</h1>
             </div>
             <div id="bottom">
-                <img src="/cutebunny.jpg" alt="Image of cute bunny wearing bunny ears" id="cute-bunny" />
+                <p>I'm a Computer Science major and Cognitive Science minor at Pomona College. I'm interested in web design, video game design, and machine learning.
+                </p>
+
+                <p>
+                    Email me at azaa2021@mymail.pomona.edu to reach me. Looking forward to connecting!
+                </p>  
             </div>
+            <hr/>
+            <h1>
+                Projects:
+            </h1>
+            <div id="flex-container">
+                <div className="project-box">
+                    <img src="/cutebunny.jpg" alt="Cute bunny"/>
+                    <h2>Cute Bunny Image</h2>
+                    <p>Created an image of a cute bunny through Photoshop.</p>
+                </div>
+                <div className="project-box">
+                    <img src="/pinksky.jpg" alt="Pink sky"/>
+                    <h2>Pink Sky</h2>
+                    <p>Coded a website that autogenerates a unique pink sky.</p>
+            </div>
+            
+            </div>
+            <p>
+
+                
+            </p>
         </div>
          
         </>
